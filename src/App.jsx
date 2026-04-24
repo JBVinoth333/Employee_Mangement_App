3678db31f4ffe18616c6b45f00a7532728129f80
JBVinoth333 <bevinoth699@gmail.com>
2026-04-24 14:56:06 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import Signup from './pages/Signup/Signup';
 import Login from './pages/Login/Login';
