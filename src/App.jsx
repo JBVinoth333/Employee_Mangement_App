b195012966984bd475b1f784ad8cf5692e61ab0a
JBVinoth333 <bevinoth699@gmail.com>
2026-04-24 16:27:08 +0530

    Files Updated

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <ThemeProvider>
-      <BrowserRouter basename="/EmployeeManagement">
+      <BrowserRouter basename={routerBase}>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
